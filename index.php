e5189ef2f46f46d904cfce91091f35f07541428d
Shivani <107976948+ShivaniVerma2498@users.noreply.github.com>
2023-07-14 11:25:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $login = $_SESSION['login'];
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
-    <meta name="author" content="" />
+    <meta name="author" content="Shivani" />
     <title>User Login System</title>
     <!-- Favicon-->
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
